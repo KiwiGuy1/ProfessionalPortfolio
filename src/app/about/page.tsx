caf649026817fbcb35f72687305e9e71d5fd8d91
KiwiGuy1 <smallninja.jay@gmail.com>
2025-09-24 16:45:21 -0500

    changes page?

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">About Us</h1>
+      <h1 className="text-4xl font-bold mb-4">About Me</h1>
       <p className="text-lg text-center max-w-2xl">
         Hi, I make beautiful website and web applications. I have a passion for
         creating engaging and interactive user experiences. My expertise
