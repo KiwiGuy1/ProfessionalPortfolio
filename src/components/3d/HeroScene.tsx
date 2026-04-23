f543dab3b7837333098fc0e4e1ba3a0dea3d826e
KiwiGuy1 <smallninja.jay@gmail.com>
2026-04-23 00:28:54 -0500

    big changes

--- src/components/3d/HeroScene.tsx
+++ src/components/3d/HeroScene.tsx
@@ -2,40 +2,28 @@
 
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Bounds } from "@react-three/drei";
 import InteractiveMesh from "./InteractiveMesh";
 
 export default function HeroScene() {
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <Canvas
-        camera={{ position: [0.15, 0.1, 3.35], fov: 34 }}
+        camera={{ position: [1.9, 0.2, 2.45], fov: 30 }}
         dpr={[1, 1.5]}
         gl={{ antialias: true, alpha: true }}
       >
         <color attach="background" args={["#0b110d"]} />
-        <ambientLight intensity={0.72} color="#fff7e8" />
-        <hemisphereLight args={["#fff4dc", "#152016", 1.1]} />
+
+        <ambientLight intensity={0.7} />
         <directionalLight
-          position={[2.4, 3.4, 4.2]}
-          intensity={1.85}
-          color="#fff2d4"
+          position={[3, 4, 4]}
+          intensity={1.35}
+          color="#f6fff2"
         />
-        <directionalLight
-          position={[-3.4, 1.2, 2.2]}
-          intensity={0.85}
-          color="#bcefff"
-        />
-        <directionalLight
-          position={[0.2, 2.6, -3.5]}
-          intensity={0.9}
-          color="#ffffff"
-        />
+        <pointLight position={[-2, 1, 3]} intensity={8} color="#95ff7a" />
 
         <Suspense fallback={null}>
-          <Bounds fit clip observe margin={1.2}>
-            <InteractiveMesh />
-          </Bounds>
+          <InteractiveMesh />
         </Suspense>
       </Canvas>
     </div>
